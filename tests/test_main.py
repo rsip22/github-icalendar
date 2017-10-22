0774cfb188bf104fa3fc05806c8820a81502ae9e
rsip22 <rsip22@openmailbox.org>
2017-10-22 21:14:29 -0200

    Adds specifications for tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -3,17 +3,21 @@ import unittest
 import vcr
 import os
 
-# vamos:
-# 1) criar uma funcao pra criar o objeto conf, jah com a API key do ambiente
-#    (depois a gente pode incrementar essa funcao pra mais coisas)
-# 2) tirar fora o VCR pq ele tah complicando as coisas...
-# 3) escrever testes para:
-#    a. sem especificar repo e label, feed pega todas issues de repos que tem acesso
-#    b. especificando repo, feed pega todas issues daquele repo
-#    c. especificando repo e label, feed pega todas issues com aquela label, daquele repo. não pega de outros repos
-#       e nem de outras labels
-#    d. especificando apenas label, feed pega todas issues com aquela label, de todos repos que tem acesso. não pega
-#       outras labels
+"""
+TODO: Write tests
+Specifications:
+a. if no repo and label in config, does the feed indeed get all issues
+from repos the user has access to?
+b. if repo in config, does the feed get all issues from that repo,
+from all labels?
+c. if repo AND label in config, does the feed get all issues with that label,
+from the repo specified - and not from other repos?
+d. if only label in config, does the feed get all issues with that label,
+from all repos?
+e. if more than one label in config, does the feed get all issues that contain
+each label?
+"""
+
 
 class GenerateIcalTest(unittest.TestCase):
     @vcr.use_cassette(filter_headers=['authorization'])
@@ -23,5 +27,3 @@ class GenerateIcalTest(unittest.TestCase):
         conf = {'api_token': os.getenv('GITHUB_TOKEN', 'fake_key')}
         ical_object = generate_ical(conf)
         self.assertIsNotNone(ical_object)
-        label_list = [l for l in ical_object.splitlines() if l.startswith('LABEL')]
-        self.assertIsNotNone(label_list)
